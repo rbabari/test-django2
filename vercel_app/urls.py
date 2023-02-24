c605ef0708c10a7cb51cd6f39269791cac02bacc
rbabari <r.babari@gmail.com>
2023-02-24 10:55:20 -0500

    test example/

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('members/', include('members.urls')),
+    #path('members/', include('members.urls')),
     path('admin/', admin.site.urls),
-    path('', include('example.urls')),
+    path('example/', include('example.urls')),
 ]
