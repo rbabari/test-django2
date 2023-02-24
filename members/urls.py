d1831f04f9dc4f874b3d9f912e5e5b5a22b1164e
rbabari <r.babari@gmail.com>
2023-02-24 11:17:44 -0500

    members 5

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,18 @@
+"""
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('members/', views.members, name='members'),
 ]
+"""
 
+
+# example/urls.py
+from django.urls import path
+from members.views import members
+
+
+urlpatterns = [
+    path('m/', members),
+]
--- members/views.py
+++ members/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.http import HttpResponse
 
-#def members(request):
-#    return HttpResponse("Hello world!")
-
 def members(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+    return HttpResponse("Hello world! from members")
+
+
