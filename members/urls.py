1fcc4bf3b99328dfd0ed18acd7f74ea40d68106d
rbabari <r.babari@gmail.com>
2023-02-24 10:51:19 -0500

    add members 3

--- members/urls.py
+++ members/urls.py
@@ -4,3 +4,4 @@ from . import views
 urlpatterns = [
     path('members/', views.members, name='members'),
 ]
+
--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('members.urls')),
+    path('members/', include('members.urls')),
     path('admin/', admin.site.urls),
     path('', include('example.urls')),
 ]
